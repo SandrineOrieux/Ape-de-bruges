c640e0b9b50c024dad2da5aeadcda3df4d7d1977
Sandrine Orieux <sandrine.orieux@sfr.fr>
2024-03-05 11:32:18 +0100

    display logo with logo file in BDD

---
+++ src/Services/LogoService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Services;
+
+use App\Entity\Logo;
+use App\Repository\LogoRepository;
+
+class LogoService
+{
+  private $repos;
+
+  public function __construct(LogoRepository $Repos)
+  {
+    $this->repos = $Repos;
+  }
+
+  public function getLogo(): Logo
+  {
+
+    $Logo = $this->repos->findOneBy(['id' => 1]);
+
+
+    return $Logo;
+  }
+  public function getFavicon(): Logo
+  {
+
+    $Logo = $this->repos->findOneBy(['id' => 2]);
+
+
+    return $Logo;
+  }
+}
